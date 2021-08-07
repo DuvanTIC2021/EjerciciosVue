880bb2660b90d7b9fe501e3b67cb2469edca88b8
Duvan Ortiz <you@example.com>
2021-08-06 20:50:10 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        //definimos las variables que vamos a utilizar
+        name: "",
+        phone: "",
+        age: 0,
+        rta: ""
+    },
+    methods: {
+       
+});
