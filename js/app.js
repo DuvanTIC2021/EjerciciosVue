4c8af2927c68e5e034c7dd1d1ffb6c8cfaaa9b86
Duvan Ortiz <you@example.com>
2021-08-07 17:26:46 -0500

    Operaciones Basicas

--- js/app.js
+++ js/app.js
@@ -2,11 +2,27 @@ var app = new Vue({
     el: "#app",
     data: {
         //definimos las variables que vamos a utilizar
-        name: "",
-        phone: "",
-        age: 0,
-        rta: ""
+        num1: null,
+        num2: null,
+        rta: null
     },
     methods: {
-       
+        sumar() {
+            this.rta = this.num1 + this.num2;
+        },
+        restar() {
+            this.rta = this.num1 - this.num2;
+        },
+        multiplicar() {
+            this.rta = this.num1 * this.num2;
+        },
+        dividir() {
+            if (this.num2 == 0) {
+                this.rta = "Error, NO se puede dividir por cero";
+            } else {
+                this.rta = this.num1 / this.num2;
+            }
+        }
+
+    },
 });
